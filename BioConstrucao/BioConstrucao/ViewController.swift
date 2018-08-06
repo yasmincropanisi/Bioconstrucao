e4362351119e12c3ea5b14eb28682796e60ac68d
Yasmin Nogueira Spadaro Cropanisi <yasmincropanisi@gmail.com>
2018-08-06 11:31:23 -0300

    Arquiteturando projeto

---
+++ BioConstrucao/BioConstrucao/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BioConstrucao
+//
+//  Created by Yasmin Nogueira Spadaro Cropanisi on 06/08/2018.
+//  Copyright © 2018 Instituto de Pesquisas Eldorado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
